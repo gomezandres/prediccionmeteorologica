e267138ff39c2f0784f430b0af12c004cc0fc53d
gomezandres <andres.e.gomez@gmail.com>
2018-11-08 22:07:30 -0300

    initial push

---
+++ src/main/java/com/ml/solarium/SolariumApplication.java
@@ -0,0 +1,12 @@
+package com.ml.solarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolariumApplication.class, args);
+	}
+}
